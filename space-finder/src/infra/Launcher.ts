43993388a6bd69916b647f280da2a004e02782b9
mangofunky <mangofunky@hotmail.com>
2023-12-04 16:23:04 +0000

    added DDB connection with API GW and Lambda #39

--- space-finder/src/infra/Launcher.ts
+++ space-finder/src/infra/Launcher.ts
@@ -12,5 +12,5 @@ const lambdaStack = new LambdaStack(app, 'LambdaStack', {
     spacesTable: dataStack.spacesTable
 })
 new ApiStack(app, 'ApiStack', {
-    helloLambdaIntegration: lambdaStack.helloLambdaIntegration
+    spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration
 })
--- space-finder/src/infra/stacks/LambdaStack.ts
+++ space-finder/src/infra/stacks/LambdaStack.ts
@@ -4,6 +4,7 @@ import { ITable } from 'aws-cdk-lib/aws-dynamodb';
 import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Runtime} from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { DynamoPutItem } from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import { Construct } from 'constructs';
 import { join } from 'path';
 
@@ -13,32 +14,29 @@ interface LambdaStackProps extends StackProps {
 
 export class LambdaStack extends Stack {
 
-    public readonly helloLambdaIntegration: LambdaIntegration
+    public readonly spacesLambdaIntegration: LambdaIntegration
 
     constructor(scope: Construct, id: string, props: LambdaStackProps) {
         super(scope, id, props)
 
 
-        const helloLambda = new NodejsFunction(this, 'HelloLambda', {
+        const spacesLambda = new NodejsFunction(this, 'SpacesLambda', {
             runtime: Runtime.NODEJS_18_X,
             handler: 'handler',
-            entry: (join(__dirname, '..','..', 'services', 'hello.ts')),
+            entry: (join(__dirname, '..','..', 'services', 'spaces' , 'handler.ts')),
             environment: {
                 TABLE_NAME: props.spacesTable.tableName
             }
         });
-
-        helloLambda.addToRolePolicy( new PolicyStatement({
+        spacesLambda.addToRolePolicy(new PolicyStatement({
             effect: Effect.ALLOW,
+            resources: [props.spacesTable.tableArn],
             actions: [
-                's3:ListAllMyBuckets',
-                's3:ListBucket'
-            ],
-            resources: ["*"] //bad practice
-
+                'dynamodb:PutItem'
+            ]
         }))
 
-        this.helloLambdaIntegration = new LambdaIntegration(helloLambda)
+        this.spacesLambdaIntegration = new LambdaIntegration(spacesLambda)
 
     }
 }
