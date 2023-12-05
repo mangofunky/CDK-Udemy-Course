76f9023c58e252e986736983313122a9131f95dc
mangofunky <mangofunky@hotmail.com>
2023-12-05 13:35:39 +0000

    Created Data Validator Function and Model

--- space-finder/test/launcher.ts
+++ space-finder/test/launcher.ts
@@ -4,11 +4,13 @@ process.env.AWS_REGION = "eu-west-1"
 process.env.TABLE_NAME = "SpaceTable-0eb3c459d0fb"
 
 handler({
-    httpMethod: 'DELETE',
-    queryStringParameters: {
-        id: '1b8e9211-a5d0-42b8-8ef8-7b39eca0fdf1'
-    },
-    // body: JSON.stringify({
-    //     location: 'Barcelona2'
-    // })
-} as any, {} as any);
+    httpMethod: 'POST',
+    // queryStringParameters: {
+    //     id: '1b8e9211-a5d0-42b8-8ef8-7b39eca0fdf1'
+    // },
+    body: JSON.stringify({
+        location: 'Barcelona2'
+    })
+} as any, {} as any).then(result =>{
+    console.log(result)
+});
