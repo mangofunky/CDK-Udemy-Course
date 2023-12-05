f605d307705ac8546b62bbb8c2c7ac98911c35e4
mangofunky <mangofunky@hotmail.com>
2023-12-05 10:14:37 +0000

    Added Scan Operation Query to DDB

--- space-finder/src/services/spaces/GetSpaces.ts
+++ space-finder/src/services/spaces/GetSpaces.ts
@@ -1,29 +1,16 @@
-import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { v4 } from "uuid";
-
 
 export async function getSpaces(event: APIGatewayProxyEvent, ddbClient: DynamoDBClient): Promise<APIGatewayProxyResult> {
-    
-    const randomId = v4();
-    const item = JSON.parse(event.body);
 
-
-    const result = await ddbClient.send(new PutItemCommand({
+    const result = await ddbClient.send(new ScanCommand({
         TableName: process.env.TABLE_NAME,
-        Item: {
-            id: {
-                S: randomId
-            },
-            location: {
-                S: item.location
-            }
-        }
+
     }));
-    console.log(result);
+    console.log(result.Items);
 
     return {
         statusCode: 201,
-        body: JSON.stringify({id: randomId})
+        body: JSON.stringify(result.Items)
     }
 }
--- space-finder/test/launcher.ts
+++ space-finder/test/launcher.ts
@@ -4,8 +4,8 @@ process.env.AWS_REGION = "eu-west-1"
 process.env.TABLE_NAME = "SpaceTable-0eb3c459d0fb"
 
 handler({
-    httpMethod: 'POST',
-    body: JSON.stringify({
-        location: 'Barcelona'
-    })
+    httpMethod: 'GET',
+    // body: JSON.stringify({
+    //     location: 'Barcelona'
+    // })
 } as any, {} as any);
