b354e93140bda17c7535ac5fc0c33e3e9575e549
mangofunky <mangofunky@hotmail.com>
2023-12-05 10:39:58 +0000

    Added GetItem with ID Operation Query to DDB

--- space-finder/test/launcher.ts
+++ space-finder/test/launcher.ts
@@ -5,6 +5,9 @@ process.env.TABLE_NAME = "SpaceTable-0eb3c459d0fb"
 
 handler({
     httpMethod: 'GET',
+    queryStringParameters: {
+        id: '1b8e9211-a5d0-42b8-8ef8-7b39eca0fdf1'
+    }
     // body: JSON.stringify({
     //     location: 'Barcelona'
     // })
